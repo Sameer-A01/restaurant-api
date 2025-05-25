030e6e160caf09ac2c12246a6e7a7af0adc764de
Sameer-A01 <tubesameer786@gmail.com>
2025-05-25 13:07:07 +0530

    Initial commit for frontend

--- src/components/EmployeeProducts.jsx
+++ src/components/EmployeeProducts.jsx
@@ -466,7 +466,7 @@ const POSPage = () => {
               whileHover={{ y: -1 }}
             >
               <Settings size={18} />
-              <span className="hidden sm:inline">Settings</span>
+              <span className="hidden sm:inline">Setting</span>
             </motion.button>
           </div>
         </div>
